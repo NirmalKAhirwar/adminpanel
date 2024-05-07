7161091f3b8d23b5932b2ea0e5ead7f5d61fc6b6
Nirmal Kumar Ahirar <ahirwarnirmal2017@gmail.com>
2024-05-07 15:06:42 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import customerRoutes from "./routes/customerRoutes.js";
+import dotenv from "dotenv";
+import purchaseOrderRoutes from "./routes/purchaseOrderRoutes.js";
+import shippingRoutes from "./routes/shippingRoutes.js";
+import AdminRoutes from "./routes/adminRoutes.js";
+
+dotenv.config();
+const MONGO_DB_URL =
+  process.env.MONGO_DB_URL ||
+  "mongodb+srv://lamrin:lamrin@adminpanelcluster.fhhlvwb.mongodb.net/";
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_DB_URL, {});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use("/api/admin", AdminRoutes);
+app.use("/api/customer", customerRoutes);
+app.use("/api/purchaseorder", purchaseOrderRoutes);
+app.use("/api/shipping", shippingRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} : http://localhost:${PORT}/`);
+});
